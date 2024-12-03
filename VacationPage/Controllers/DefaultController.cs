111cd136ab0049f82276b0202bc50465c9c2a2f1
emrecan <turgul2606@gmail.com>
2024-12-03 23:39:24 +0300

    Add project files.

---
+++ VacationPage/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VacationPage.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VacationPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VacationPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
